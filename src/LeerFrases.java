c586e9d7c467255a6f1eef90ea3fc95ab076d77c
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-17 14:22:53 +0100

    Update LeerFrases.java

--- src/LeerFrases.java
+++ src/LeerFrases.java
@@ -1,13 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class LeerFrases {
+    public static void main(String[] args) {
+
+        //leemos el archivo de la ruta
+        try (Scanner lector = new Scanner(new File("frases.txt"))) {
+
+            // recorremos el archivo y mostramos las frases
+            while (lector.hasNextLine()) {
+                // Leemos la frase completa hasta el salto de línea
+                String frase = lector.nextLine();
+                System.out.println(frase);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: No se pudo leer 'frases.txt'. Verifica que el archivo exista.");
+        }
+    }
 }
